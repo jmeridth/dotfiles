a2f750e9ab35d638c200d7791ab50b0d7a1ad211
Jason Meridth <Jason.Meridth@rackspace.com>
2016-06-14 22:06:38 -0500

    Add setup.sh script
    - sets up symlinks

---
+++ setup.sh
@@ -0,0 +1,13 @@
+DOTFILES = ~/dev/dotfiles
+
+# ALIASES
+ln -sf $DOTFILES/jmaliases $HOME/.jmaliases
+
+# GIT
+ln -sf ~/dev/dotfiles/gitconfig $HOME/.gitconfig
+ln -sf ~/dev/dotfiles/git-completion.bash $HOME/.git-completion.bash
+ln -sf ~/dev/dotfiles/git-prompt.sh $HOME/.git-prompt.sh
+
+# TMUX
+ln -sf ~/dev/dotfiles/tmux.conf $HOME/.tmux.conf
+
