48238beae9ec2a99f15b7fdb54e77c7dcb7c1e63
jmeridth <jmeridth@gmail.com>
2016-07-14 08:40:07 -0500

    Change to update all scripts
    - git
    - tmuxinator

--- update_scripts.sh
+++ update_scripts.sh
@@ -2,3 +2,4 @@
 
 curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+curl -O https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.bash
