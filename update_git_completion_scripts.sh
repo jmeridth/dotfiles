4632a52e253c73451cdac164c0c0da9f1e916cba
jmeridth <jmeridth@gmail.com>
2016-07-14 08:38:56 -0500

    Add script to update git scripts from git repo

---
+++ update_git_completion_scripts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
