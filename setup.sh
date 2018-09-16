ccabc71614feeabb3bf7bf67404462780bf32a34
jmeridth <jmeridth@gmail.com>
2018-09-15 20:09:46 -0500

    add symlink to fix neovim

--- setup.sh
+++ setup.sh
@@ -38,6 +38,7 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +q +q!
 
+ln -sf $HOME/.config/nvim $HOME/.vim
 
 # BASH_PROFILE
 ln -sf $DOTFILES/bash_profile $HOME/.bash_profile
