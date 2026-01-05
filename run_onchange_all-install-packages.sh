64eecfbe02859286b4b8d8c28d929bee4e5ebd2c
jmeridth <jmeridth@gmail.com>
2026-01-04 18:14:14 -0600

    fix: chezmoi apply
    - remove deprecated homebrew taps
    - move content back to zshrc and templatize for checking if darwin (macos)
    - update packages scripts to only install packages nothing more
    
    Signed-off-by: jmeridth <jmeridth@gmail.com>

--- run_onchange_all-install-packages.sh
+++ run_onchange_all-install-packages.sh
@@ -5,5 +5,3 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   # oh-my-zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
-
-exec $SHELL
