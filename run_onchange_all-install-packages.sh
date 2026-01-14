0038aab24559fbf5bf84d95ebca2069eefe87dec
jmeridth <jmeridth@gmail.com>
2026-01-14 11:46:38 -0600

    fix: use command -v instead of which
    Signed-off-by: jmeridth <jmeridth@gmail.com>

--- run_onchange_all-install-packages.sh
+++ run_onchange_all-install-packages.sh
@@ -6,7 +6,7 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-if which code >/dev/null 2>&1; then
+if command -v code >/dev/null 2>&1; then
   echo "Updating vscode to allow for holding down keys in vscode for vim ..."
   defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 fi
